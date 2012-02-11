786a69f958349d0720cf96c2d4208ccdd2888577
sknagesh <sknagesh@gmail.com>
2012-02-11 18:37:35 +0530

    first commit

---
+++ add_part.php
@@ -0,0 +1,46 @@
+<?php
+include('dewdb.inc');
+$cxn = mysql_connect($dewhost,$dewname,$dewpswd) or die(mysql_error());
+mysql_select_db('Process',$cxn) or die("error opening db: ".mysql_error());
+print_r($_POST);
+print_r($_FILES);
+
+
+foreach ($_FILES as $key => $value) {
+	print("$key = $value");
+}
+/*
+if(isSet($_FILES))
+{
+	$fileName = $_FILES['drg']['name'];
+	$tmpName = $_FILES['drg']['tmp_name'];
+	$fileSize = $_FILES['drg']['size'];
+	$fileType = $_FILES['drg']['type'];
+print("file name=$fileName temp name=$tmpName file size=$fileSize type=$fileType");
+}
+/*
+$custid=$_POST['Customer_ID'];
+$drawingno=$_POST['drawingno'];
+$componentname=$_POST['componentname'];
+$price=$_POST['price'];
+
+
+$query="INSERT INTO Components (Customer_ID,Drawing_NO,Component_Name,Price) ";
+$query.="VALUES('$custid','$drawingno','$componentname','$price');";
+
+print($query);
+
+$res=mysql_query($query) or die(mysql_error());
+
+$result=mysql_affected_rows();
+if($result!=0)
+{
+print("Added new Component $componentname with a price of $price");	
+	
+}else
+	{
+		print("Error Adding");
+	}
+
+*/
+?>
---
+++ get_customer.php
@@ -0,0 +1,20 @@
+<?php
+include('dewdb.inc');
+$cxn = mysql_connect($dewhost,$dewname,$dewpswd) or die(mysql_error());
+mysql_select_db('Process',$cxn) or die("error opening db: ".mysql_error());
+//print_r($_POST);
+$query="SELECT * FROM Customer;";
+
+print("<select name=\"Customer_ID\" id=\"Customer_ID\" class=\"required\">");
+echo '<option value="">Select Customer</option>';
+$resa = mysql_query($query, $cxn) or die(mysql_error($cxn));
+while ($row = mysql_fetch_assoc($resa))
+{
+echo "<option value=".$row['Customer_ID'].">";
+echo "$row[Customer_Name]</option>";
+}
+print("</select></td></tr>");
+
+
+
+?>
